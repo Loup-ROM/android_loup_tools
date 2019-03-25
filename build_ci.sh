17b1e13b17c2b0f31cf2cb620dab642645e3a930
bitrvmpd <e.noyer.silva@gmail.com>
2019-03-24 20:50:41 -0600

    :spakles: New build for ci

---
+++ build_ci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ${WORKSPACE}
+/usr/bin/repo sync -c -n --force-sync -j 16 && /usr/bin/repo sync -c -l -j 16 
+chmod +x loup/tools/build.sh 
+loup/tools/build.sh santoni $1
+
+# Upload our new build
+sshpass -p "$LOBO" scp -o "StrictHostKeyChecking no" '$WORKSPACE/out/target/product/$1/lineage-*.zip.md5sum' $SF:$PROJECT/LOS141
+sshpass -p "$LOBO" scp -o "StrictHostKeyChecking no" '$WORKSPACE/out/target/product/$1/lineage-*.zip' $SF:$PROJECT/LOS141
+
+# Create delta
+
+
