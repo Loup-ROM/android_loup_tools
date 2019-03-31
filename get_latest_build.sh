7fa764ae6c326455400fe9a180caa6e308194718
bitrvmpd <e.noyer.silva@gmail.com>
2019-03-30 19:48:42 -0600

    Done debugging

--- get_latest_build.sh
+++ get_latest_build.sh
@@ -6,15 +6,14 @@ export DEVICE=$1
 jq '.["./'$DEVICE'"] | .[-1] | .["filename"]' android_loup_ota/devices.json
 LATEST=$(jq '.["./'$DEVICE'"] | .[-1] | .["filename"]' android_loup_ota/devices.json)
 export LATEST=$(echo "${LATEST//\"}")
-echo $LATEST > latest-file
-
 
 # Create delta folder structure
-#rm -rf $WORKSPACE/deltas
-#mkdir -p $WORKSPACE/deltas/last/$DEVICE
-#mkdir -p $WORKSPACE/deltas/current/$DEVICE
+rm -rf $WORKSPACE/deltas
+mkdir -p $WORKSPACE/deltas/last/$DEVICE
+mkdir -p $WORKSPACE/deltas/current/$DEVICE
 
 # Download latest and preserve filename
+echo $LATEST > latest-file
 cd $WORKSPACE/deltas/current/$DEVICE
 curl -L -O -J https://sourceforge.net/projects/loup-rom/files/LOS-14.1/$LATEST/download
 cd -
