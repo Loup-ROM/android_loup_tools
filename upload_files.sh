c448a23872eb2ca9f9f433faf8fa84f6d4445e29
bitrvmpd <e.noyer.silva@gmail.com>
2019-03-30 20:07:16 -0600

    Fixing filenames

--- upload_files.sh
+++ upload_files.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DEVICE=$1
-FILENAME=$2
-sshpass -p $sfpass scp -o "StrictHostKeyChecking no" "$WORKSPACE/deltas/jni/publish/$DEVICE/$FILENAME.sign" "$sfusername:$sfproject/LOS-14.1/deltas/$DEVICE"
-sshpass -p $sfpass scp -o "StrictHostKeyChecking no" "$WORKSPACE/deltas/jni/publish/$DEVICE/$FILENAME.delta" "$sfusername:$sfproject/LOS-14.1/deltas/$DEVICE"
-sshpass -p $sfpass scp -o "StrictHostKeyChecking no" "$WORKSPACE/deltas/jni/publish/$DEVICE/$FILENAME.update" "$sfusername:$sfproject/LOS-14.1/deltas/$DEVICE"
-sshpass -p $sfpass scp -o "StrictHostKeyChecking no" "$WORKSPACE/out/target/product/$DEVICE/$FILENAME.zip.md5sum" "$sfusername:$sfproject/LOS-14.1"
-sshpass -p $sfpass scp -o "StrictHostKeyChecking no" "$WORKSPACE/deltas/current/$DEVICE/$FILENAME.zip" "$sfusername:$sfproject/LOS-14.1"
+FILENAME_OLD=$2
+FILENAME_NEW=$3
+sshpass -p $sfpass scp -o "StrictHostKeyChecking no" "$WORKSPACE/deltas/jni/publish/$DEVICE/$FILENAME_OLD.sign" "$sfusername:$sfproject/LOS-14.1/deltas/$DEVICE"
+sshpass -p $sfpass scp -o "StrictHostKeyChecking no" "$WORKSPACE/deltas/jni/publish/$DEVICE/$FILENAME_OLD.delta" "$sfusername:$sfproject/LOS-14.1/deltas/$DEVICE"
+sshpass -p $sfpass scp -o "StrictHostKeyChecking no" "$WORKSPACE/deltas/jni/publish/$DEVICE/$FILENAME_OLD.update" "$sfusername:$sfproject/LOS-14.1/deltas/$DEVICE"
+sshpass -p $sfpass scp -o "StrictHostKeyChecking no" "$WORKSPACE/out/target/product/$DEVICE/$FILENAME_NEW.zip.md5sum" "$sfusername:$sfproject/LOS-14.1"
+sshpass -p $sfpass scp -o "StrictHostKeyChecking no" "$WORKSPACE/deltas/current/$DEVICE/$FILENAME_NEW.zip" "$sfusername:$sfproject/LOS-14.1"
